067cfb06ef9972c16f35f14f3eb7a08de0db2c58
Nabid <nabid.explore@gmail.com>
2024-03-26 09:19:18 +0600

    sign-up button added to the home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,19 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div className="h1-bold">Home</div>;
+  return (
+    <div className="h1-bold flex-center min-h-screen flex-col gap-9">
+      Home
+      <SignedOut>
+        <Link href="/sign-up">Sign Up</Link>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default page;
