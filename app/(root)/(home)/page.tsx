6a4ea4b5cc20fcf405def607d03e70a3b726e948
Nabid <nabid.explore@gmail.com>
2024-03-31 12:12:42 +0600

    navbar is complete

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,7 @@
-import { SignedIn, UserProfile } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className=" flex-center min-h-screen flex-col gap-9">
-      Home
-      <SignedIn>
-        <UserProfile />
-      </SignedIn>
-    </div>
-  );
+  return <div className=" ">Home</div>;
 };
 
 export default page;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,21 @@
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Navbar from "@/components/shared/nabvar/Navbar";
+import RightSidebar from "@/components/shared/RightSidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="background-light850_dark100 relative">
+      <Navbar />
+      <div className="flex">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        <RightSidebar />
+      </div>
+    </main>
+  );
 };
 
 export default layout;
